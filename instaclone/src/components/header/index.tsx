a25d426c2ec6d95349e7ebd446a9b0c66cc7fdf6
Perkles <deivyssonlucas30@gmail.com>
2023-09-18 10:09:45 -0300

    React Native: clone do instagram

---
+++ instaclone/src/components/header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá Mundo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+});
